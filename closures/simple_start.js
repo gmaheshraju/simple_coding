d436a120a8faf044bfd162c3d0f5d492ff981d97
Mahesh Guntumadugu <g.maheshlearn@outlook.com>
2024-03-01 23:22:36 +0530

    coding challenges

---
+++ closures/simple_start.js
@@ -0,0 +1,11 @@
+// closure example
+function example() {
+  let myWebsiteName = "curiousengineers.in";
+
+  function displaySiteName() {
+    console.log(myWebsiteName);
+  }
+  displaySiteName();
+}
+
+example();
